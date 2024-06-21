9dc99fa9fc0129183e0eab38b9e0befcfda1296b
Nick Snyder <30705806+nsnyder1992@users.noreply.github.com>
2024-06-21 07:02:53 -0500

    Update footer.tsx

--- components/misc/footer.tsx
+++ components/misc/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="container mx-auto px-5">
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            <PreviewLink href="/table-of-contents">How to Play</PreviewLink> play LittleP6.
+            <PreviewLink href="/table-of-contents">How to Play</PreviewLink> LittleP6.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
